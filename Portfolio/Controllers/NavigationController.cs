3cf4b2bf44800848612698cb6e053699b83f12fa
Hasib <hasibulhasan2009@gmail.com>
2018-11-05 16:27:45 +0600

    works

---
+++ Portfolio/Controllers/NavigationController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class NavigationController : Controller
+    {
+        // GET: Navigation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Works()
+        {
+            return View();
+        }
+
+        public ActionResult Blogs()
+        {
+            return View();
+        }
+    }
+}
